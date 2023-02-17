a99e4cb2177eb13f34d433d95fc59ba462063e98
alfredomarquez0 <alfredoemc.1709@gmail.com>
2023-02-16 23:47:52 -0300

    preentrega2

--- main.js
+++ main.js
@@ -1,81 +1,95 @@
+const productos =[
+    {id: 1, nombre: "clasica", precio: 300 },
+    {id: 2, nombre: "choco", precio: 300 },
+    {id: 3,nombre: "nutella", precio: 400 },
+    {id: 4,nombre: "avena", precio: 250 }
+]
+
+const comprador=[]
+let carrito=[]
+
+function datosEntrega(){
+    let nombre= prompt('Ingrese su nombre')
+    let entrega= prompt ("Ingrese \n 1 si retira en tienda \n 2 si es delivery")
+    if((nombre != "") && (entrega != "")){
+        comprador.push(nombre)
+        alert("sus datos quedaron guardados como:" + comprador.join("\n"))
+        while((entrega!="0") && (entrega !="")){
+            switch (entrega) {
+                case "1":
+                alert("Entrega en tienda")
+                break;
+        
+                case "2":
+                alert("Envio con delivery")
+                let direccion = prompt("Ingresa la direccion del destino de la orden")
+                alert("Tu orden ira a: " + direccion)
+                break;
+        
+                default:
+                    alert("Opcion invalida")
+                    break;
+            }
+            entrega= prompt ("Escriba \n 0 para continuar")
+        }
+}
+}
+
 alert("Bienvenido a Deli Cookies") 
-let nombre = prompt("Para comenzar tu pedido, ingresa tu nombre");
-alert("Como va? " + nombre + ", comencemos!")
-let entrega= prompt ("Ingrese \n 1 si retira en tienda \n 2 si es delivery");
-const galletaClasica = 300;
-const galletaChoco = 300;
-const galletaNutella = 400;
-const galletaAvena = 250;
-const galletaDdl =  300;
-let precioTotal = 0;
+datosEntrega()
 
-function precio (dato1, dato2) {
-    resultado = dato1 * dato2
-    return resultado
+    let seleccion = prompt('Desea comenzar la compra \n 1 si \n 2 no')
+    
+    while(seleccion!='1' && seleccion!='2'){
+    alert('Ingrese \n 1 o \n 2')
+    seleccion= prompt('Desea comenzar la compra \n 1 si \n 2 no')
 }
 
-while((entrega!="0") && (entrega !="")){
-    switch (entrega) {
-        case "1":
-        alert("Entrega en tienda")
-        break;
+        if(seleccion=='1'){
+            alert('estos son nuestros productos')
+            let galletas = productos.map((producto) => producto.nombre + '/' + '$' + producto.precio )
+            alert(galletas.join(','))
+        } else if(seleccion =='2'){
+            alert('Gracias por visitarnos')
+        }
+while(seleccion !='2'){
+    let cargarCarrito = prompt('Anade un producto al carrito \n 1 Clasica \n 2 Chocolate \n 3 Nutella \n 4 Avena ')
 
-        case "2":
-        alert("Envio con delivery")
-        let direccion = prompt("Ingresa la direccion del destino de la orden")
-        alert("Tu orden ira a: " + direccion)
-        break;
+    const validProd= productos.find(el => el.id == cargarCarrito)
 
+    let precio=0
+
+    switch (cargarCarrito) {
+        case '1':
+            precio=productos[0].precio
+            break;
+
+        case '2':
+            precio=productos[1].precio
+            break;
+
+        case '3':
+            precio=productos[2].precio
+            break;
+
+        case '4':
+            precio=productos[3].precio
+            break;
+    
         default:
-            alert("Opcion invalida")
             break;
     }
-    entrega= prompt ("Escriba \n 0 para continuar")
-}
 
-let pedido = entrega
+    carrito.push({validProd, precio})
 
-if (pedido == "0") {
-    let sabores = prompt("Por favor, seleccione los sabores de las galletas \n 1 Clasica \n 2 Chocolate \n 3 Nutella \n 4 Avena \n 5 Dulce de leche \n x para finalizar")
-    while (sabores != "x") {
-        switch (sabores){
-            case "1":
-            let cantidadClasica = prompt("Ingrese la cantidad");
-            precioTotal = parseInt(precioTotal + precio(galletaClasica, cantidadClasica));
-            alert ("El precio total es " + "$ " + precioTotal);
-            break;
+    seleccion= prompt('Desea continuar comprando \n 1 si \n 2 no')
 
-            case "2":
-            let cantidadChoco = prompt("Ingrese la cantidad");
-            precioTotal = parseInt(precioTotal + precio(galletaChoco, cantidadChoco));
-            alert ("El precio total es " + "$ " + precioTotal);
-            break;
-
-            case "3":
-                let cantidadNutella = prompt("Ingrese la cantidad");
-                precioTotal = parseInt(precioTotal + precio(galletaNutella, cantidadNutella));
-                alert ("El precio total es " + "$ " + precioTotal);
-                break;
-
-            case "4":
-                let cantidadAvena = prompt("Ingrese la cantidad");
-                precioTotal = parseInt(precioTotal + precio(galletaAvena, cantidadAvena));
-                alert ("El precio total es " + "$ " + precioTotal);
-                break;
-
-            case "5":
-                let cantidadDdl = prompt("Ingrese la cantidad");
-                precioTotal = parseInt(precioTotal + precio(galletaDdl, cantidadDdl));
-                alert ("El precio total es " + "$ " + precioTotal);
-                break;
-
-            default:
-            alert("Opcion no valida");
-            break;
-    }
-    sabores = prompt("Por favor, seleccione los sabores de las galletas \n 1 Clasica \n 2 Chocolate \n 3 Nutella \n 4 Avena \n 5 Dulce de leche \n x para finalizar");
+    while(seleccion =='2'){
+        carrito.forEach((compraFinal) => {
+            alert(`Producto: ${compraFinal.validProd.nombre}, Precio: ${compraFinal.precio}`)
+        })
+        break
     }
 }
-
 
 
